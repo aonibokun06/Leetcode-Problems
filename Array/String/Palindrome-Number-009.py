dd687a0ba4ccef6a7b75fa5ae133c4a4ea85688c
Ayomide Onibokun <aonibokun06@gmail.com>
2025-03-08 19:21:50 -0500

    Update Palindrome-Number-009.py

--- Array/String/Palindrome-Number-009.py
+++ Array/String/Palindrome-Number-009.py
@@ -1,12 +1,12 @@
 # Given an integer x, return true if x is a palindrome, and false otherwise.
 
-    def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        l = 0
-        r = len(x)-1
-        while l < r:
-            if x[l] != x[r]:
-                return False
-            l+=1
-            r-=1
-        return True
+def isPalindrome(self, x: int) -> bool:
+    x = str(x)
+    l = 0
+    r = len(x)-1
+    while l < r:
+        if x[l] != x[r]:
+            return False
+        l+=1
+        r-=1
+    return True
