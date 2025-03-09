b82bcdd49df184f274d5202f8550798510406ec7
Ayomide Onibokun <aonibokun06@gmail.com>
2025-03-08 19:04:06 -0500

    Delete Two-Sum-001.py

--- Two-Sum-001.py
+++
@@ -1,9 +0,0 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    table = {}
-    n = len(nums)
-    for i, num in enumerate(nums):
-        diff = target - num
-        if diff in table:
-            return [i, table[diff]]
-        table[num] = i
-    return []
